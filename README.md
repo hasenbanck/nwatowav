36bc6dab2a1db64a60d8f46ac4acde6b8323065b
Nils Hasenbanck <nils@hasenbanck.de>
2012-12-11 19:17:35 +0100

    Working converted I used to test the NWA library.
    It can convert NWA,NVK and OVK files.
    
    Signed-off-by: Nils Hasenbanck <nils@hasenbanck.de>

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 nwatowav
 ========
 
-Converts NWA to WAV files. Written in GO and uses the go-rlvm/nwa library.
+Converts NWA/NWK to WAV files and OVK to OGG files. Written in GO and uses
+the go-rlvm/nwa library.
+
+## REQUIREMENTS
+This programm was written in GO 1.0.3 and needs the github.com/go-rlvm/nwa
+library.
+
+## INSTALLATION
+git clone github.com/go-rlvm/nwatowav
+cd nwatowav
+go build
+./nwatowav -h
+
+## LICENSE
+This programm is licensed under the GNU General Public License version 3
+(or later). You can find a copy of the license in the GPL.txt.
+
+It was written while looking at the nwatowav programm written
+by Kazunori "jagarl" Ueno, which has a BSDish license.
---
+++ nwatowav.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"github.com/go-rlvm/nwa"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+var inputfile = flag.String("inputfile", "", "path to the input file.")
+
+type FileType int
+
+const (
+	NONE  FileType = iota
+	NWA
+	NWK
+	OVK
+)
+
+func main() {
+	flag.Parse()
+
+	if *inputfile == "" {
+		log.Fatal("You need to define an input file!")
+	}
+
+	file, err := os.Open(*inputfile)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var outfilename, outext, outpath string
+	var filetype FileType
+	var headblksz int64
+
+	switch {
+	case strings.Contains(*inputfile, ".nwa"):
+		{
+			filetype = NWA
+			outfilename = strings.TrimRight(*inputfile, ".nwa")
+			outext = "wav"
+		}
+	case strings.Contains(*inputfile, ".nwk"):
+		{
+			filetype = NWK
+			outfilename = strings.TrimRight(*inputfile, ".nwk")
+			headblksz = 12
+			outext = "wav"
+		}
+	case strings.Contains(*inputfile, ".ovk"):
+		{
+			filetype = OVK
+			outfilename = strings.TrimRight(*inputfile, ".ovk")
+			headblksz = 16
+			outext = "ogg"
+		}
+	}
+	if filetype == NONE {
+		log.Fatal("This program can only handle .nwa/.nwk/.ovk files right now.")
+	}
+
+	if filetype == NWA {
+		var data io.Reader
+		if data, err = nwa.DecodeAsWav(file); err != nil {
+			log.Fatal(err)
+		}
+
+		outpath = fmt.Sprintf("%s.%s", outfilename, outext)
+
+		var out *os.File
+		out, err = os.Create(outpath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		if _, err = io.Copy(out, data); err != nil {
+			log.Fatal(err)
+		}
+	} else { // NWK or OVK files
+		var indexcount int32
+		binary.Read(file, binary.LittleEndian, &indexcount)
+		if indexcount <= 0 {
+			if filetype == OVK {
+				log.Fatalf("Invalid Ogg-ovk file: %s: index = %d\n", inputfile, indexcount)
+			} else {
+				log.Fatalf("Invalid Koe-nkw file: %s: index = %d\n", inputfile, indexcount)
+			}
+		}
+
+		tblsiz := make([]int32, indexcount)
+		tbloff := make([]int32, indexcount)
+		tblcnt := make([]int32, indexcount)
+		tblorigsiz := make([]int32, indexcount)
+
+		var i int32
+		for i=0;i<indexcount;i++ {
+			buffer := new(bytes.Buffer)
+			if count, err := io.CopyN(buffer, file, headblksz); count != headblksz || err != nil {
+				log.Fatal("Couldn't read the index entries!")
+			}
+			binary.Read(buffer, binary.LittleEndian, &tblsiz[i])
+			binary.Read(buffer, binary.LittleEndian, &tbloff[i])
+			binary.Read(buffer, binary.LittleEndian, &tblcnt[i])
+			binary.Read(buffer, binary.LittleEndian, &tblorigsiz[i])
+		}
+
+		for i=0;i<indexcount;i++ {
+			if tbloff[i] <= 0 || tblsiz[i] <= 0 {
+				log.Fatalf("Invalid table[%d]: cnt %d, off %d, size %d\n", i, tblcnt[i], tbloff[i], tblsiz[i])
+				continue
+			}
+
+			var outdata io.Reader
+			buffer := new(bytes.Buffer)
+			file.Seek(int64(tbloff[i]), 0)
+			if count, err := io.CopyN(buffer, file, int64(tblsiz[i])); count != int64(tblsiz[i]) || err != nil {
+				log.Fatalf("Couldn't read the data for table[%d]: cnt %d, off %d, size %d\n", i, tblcnt[i], tbloff[i], tblsiz[i])
+			}
+
+			if filetype == NWK {
+				if outdata, err = nwa.DecodeAsWav(buffer); err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				outdata = buffer
+			}
+
+			outpath = fmt.Sprintf("%s-%d.%s", outfilename, tblcnt[i], outext)
+
+			var out *os.File
+			out, err = os.Create(outpath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer out.Close()
+
+			if _, err = io.Copy(out, outdata); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+	}
+}
